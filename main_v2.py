0381c483af2a85eb8812d4963986f1e4bb670fe9
HaDassz <mchotdog90631@gmail.com>
2022-11-18 17:52:37 +0800

    1111118 TextField font setting success

--- main_v2.py
+++ main_v2.py
@@ -10,10 +10,13 @@ import threading
 from threading import Event
 from kivymd.app import App, MDApp
 from kivy.lang.builder import Builder
+from kivy.core.text import LabelBase
 from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.image import Image
-from kivymd.uix.textfield import textfield
+from kivymd.uix.textfield.textfield import MDTextField
+from kivy.uix.textinput import TextInput
+from kivymd.uix.relativelayout import MDRelativeLayout
 from kivy.metrics import dp
 from kivy.animation import Animation
 from kivy.uix.widget import Widget
@@ -22,9 +25,25 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.audio import SoundLoader
 
+
+LabelBase.register(name="msjh", fn_regular="./fonts/msjh.ttc")
 class ImageButton(ButtonBehavior, Image):
     def on_press(self):
         print("This image button is pressed!")
+        
+class MyTextField(MDTextField):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.font_name = "./fonts/msjh.ttc"
+        self.font_name_helper_text = "./fonts/msjh.ttc"
+        print(self.font_name_helper_text)
+        
+class MyTextInput(TextInput):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.font_name = 'msjh'
+        self.te
+        
 class WelcomePage1(Screen):
     pass
 
@@ -40,10 +59,12 @@ class TempShow1(Screen):
 class TempShow2(Screen):
     pass
 
+# global variables
+stage1_correct_numbers = []
+
 class FirstStage(Screen):
     my_text = StringProperty('開始')
     event = Event()
-    stage1_correct_numbers = []
     def on_button_click(self):
         # Start a new thread. Be aware of creating new thread every time you press the button.
         # In which case you need to keep a reference or track the created thread.
@@ -53,20 +74,20 @@ class FirstStage(Screen):
         thread1.start()
 
     def do_button_click(self, event):
-        self.stage1_correct_numbers = []
-        ROUND_ = 7
+        stage1_correct_numbers = []
+        ROUND_ = 6
         for i in range(ROUND_ + 1):
             if event.is_set():
                 self.my_text = u"開始"
                 break
-            if i == ROUND_ -1:
+            if i == ROUND_:
                 self.my_text = "結束"
                 break
             else:
                 number = random.randint(1,9)
-                self.stage1_correct_numbers.append(number)
+                stage1_correct_numbers.append(number)
                 print(number)
-                print(self.stage1_correct_numbers)
+                print(stage1_correct_numbers)
                 self.my_text = str(number)
                 if number == 1:
                     self.play_sound("./audio/教育部辭典1.mp3")
@@ -102,6 +123,14 @@ class FirstStage(Screen):
         sound = SoundLoader.load(filename)
         if sound:
             sound.play()
+
+class NumberInput1(Screen):
+    # answer_one = StringProperty()
+    # print(answer_one)
+    
+    def show_data(self):
+        print(self.ids.answer_one.text, self.ids.answer_two.text, self.ids.answer_three.text,)
+    
 
 class SecondStage(Screen):
     my_text = StringProperty('Start')
@@ -157,6 +186,8 @@ class SecondStage(Screen):
         if sound:
             sound.play()
 
+
+
 # Create the screen manager
 sm = ScreenManager()
 sm.add_widget(WelcomePage1(name='welcom_page1'))
@@ -165,11 +196,14 @@ sm.add_widget(Instruction1(name='instruction1'))
 sm.add_widget(TempShow1(name='temp_show1'))
 sm.add_widget(TempShow2(name='temp_show2'))
 sm.add_widget(FirstStage(name='first_stage'))
+sm.add_widget(NumberInput1(name='number_input1'))
 sm.add_widget(SecondStage(name='second_stage'))
 
 class DemoApp(MDApp):
     def build(self):
+        self.theme_cls.primary_palette = "Red"
         screen = Builder.load_file("./TheLab_v2.kv")
         return screen
 
-DemoApp().run()
+if __name__ == "__main__":
+    DemoApp().run()
